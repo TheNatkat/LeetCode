4ac51b4da1ef7b63d58018e494150e9f583e1f30
okeyd0ki <104088023+Okeyd0ki@users.noreply.github.com>
2022-10-19 17:19:54 +0530

    Time: 3 ms (64.68%), Space: 41.9 MB (69.81%) - LeetHub

---
+++ 2315-count-asterisks/2315-count-asterisks.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countAsterisks(String s) {
+        int count=0;
+        int ans=0;
+        for(int i=0; i<s.length(); i++){
+            if (s.charAt(i)=='|') count++;
+            if(count==0 || count%2==0){
+                if (s.charAt(i)=='*') ans++;
+            }
+        }
+        return ans;
+    }
+}
