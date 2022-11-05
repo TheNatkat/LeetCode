e6e5c7476137d0ceea8c5887eb24c8b4fec8a930
okeyd0ki <104088023+Okeyd0ki@users.noreply.github.com>
2022-11-05 17:50:32 +0530

    Time: 2 ms (88.27%), Space: 55.7 MB (7.56%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int zeros=0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]==0){
+                zeros++;
+            }else if(zeros>0){
+                int temp=nums[i];
+                nums[i]=0;
+                nums[i-zeros]=temp;
+            }
+        }
+        
+    }
+}
